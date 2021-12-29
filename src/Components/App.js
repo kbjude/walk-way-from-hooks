8785ccd7753026fa1cc131d55cafaa3e6c52a253
jkajura <jkajura@innovationvillage.co.ug>
2021-12-29 21:55:33 +0300

    WIP classes

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,15 +4,31 @@ import Header from './Header';
 import Greetings from './Greeting';
 
 class App extends React.Component {
+
+  constructor (){
+    super()
+    this.state = {
+      name:"Joram Bagonza",
+      food:"Animal meat",
+      isLoggedIn : false
+    }
+  }
   render() {
+    let wordDisplay
+    if (this.state.isLoggedIn===true){
+      wordDisplay= "In"
+    } else {
+      wordDisplay= "out"
+    }
+
     const styles = {
       color: "red"
     }
   return (
     <div className="App">
-
-        <Greetings />
-        <Header username='Testing' />
+      <h1> You are currently logged {wordDisplay}</h1>
+        <Greetings  />
+        <Header username={this.state.name}  />
     </div>
   );
 }
